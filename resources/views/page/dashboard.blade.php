487973c07ff7ee627bf1eb005576e68e0505899e
agungpens <aldyryuza@it.student.pens.ac.id>
2024-06-20 16:29:27 +0700

    update dashboard

--- resources/views/page/dashboard.blade.php
+++ resources/views/page/dashboard.blade.php
@@ -1,31 +1,4 @@
 <div class="row">
-
-    <div class="col-lg-12 mb-4 order-0">
-        <div class="card">
-            <div class="d-flex align-items-end row">
-                <div class="col-sm-7">
-                    <div class="card-body">
-                        <h5 class="card-title text-primary">Selamat Datang {{ session('nama') }} 🎉</h5>
-                        <p class="mb-4">
-                            You have done <span class="fw-bold">72%</span> more sales today. Check your new badge in
-                            your profile.
-                        </p>
-                        <a class="btn btn-sm btn-outline-primary" href="#" onclick="myProfile(this)"
-                            data_id="{{ session('id') }}">
-
-                            <span class="align-middle">My Profile</span>
-                        </a>
-
-                    </div>
-                </div>
-                <div class="col-sm-5 text-center text-sm-left">
-                    <div class="card-body pb-0 px-0 px-md-4">
-                        <img src="{{ asset('assets/img/illustrations/man-with-laptop-light.png') }}" height="140" />
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
     <div class="col-12 mb-4">
         @if (isset($mou_yang_akan_habis) && $mou_yang_akan_habis != [])
         <div class="alert alert-warning"><b>
